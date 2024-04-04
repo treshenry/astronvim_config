0bbcdd6eee6f96b051648116b721b6bb1becc2c2
Tres Henry <tres@treshenry.net>
2024-04-04 15:14:07 +0200

    Initial commit

---
+++ lua/community.lua
@@ -0,0 +1,13 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.elixir-phoenix" },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  -- import/override with your plugins folder
+}
